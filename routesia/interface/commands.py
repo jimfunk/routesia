42ac43f11f1d6a179116e9dd00af42f6cca86452
James Oakley <jfunk@funktronics.ca>
2021-10-19 08:30:01 -0300

    Add missing imports

--- routesia/interface/commands.py
+++ routesia/interface/commands.py
@@ -4,8 +4,10 @@ routesia/interface/commands.py - Routesia interface commands
 from routesia.cli.command import CLICommand, CLICommandSet
 from routesia.cli.parameters import (
     Bool,
+    Compound,
     Int32,
     IPAddress,
+    List,
     ProtobufEnum,
     String,
     UInt16,
